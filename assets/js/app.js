81fbe5196f3b11474d4d0036adb777ef1b2cfe42
Zach Taylor <zachtylr04@gmail.com>
2024-11-16 14:28:57 -0700

    Remove htmx for now

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,51 +15,52 @@
 //     import "some-package"
 //
 
-import "../vendor/htmx.min.js"
-
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
+import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+import topbar from "../vendor/topbar";
 
-let Hooks = {}
+let Hooks = {};
 
 Hooks.GoBack = {
   mounted() {
-    this.el.addEventListener("click", e => {
-      window.history.back()
-    })
-  }
-}
+    this.el.addEventListener("click", (e) => {
+      window.history.back();
+    });
+  },
+};
 
 Hooks.OptimisticRemove = {
   mounted() {
-    this.el.addEventListener("click", e => {
-      let { removeId, event, ...payload } = this.el.dataset
-      let elToRemove = document.getElementById(removeId)
-      elToRemove.remove()
-      this.pushEvent(event, payload)
-    })
-  }
-}
+    this.el.addEventListener("click", (e) => {
+      let { removeId, event, ...payload } = this.el.dataset;
+      let elToRemove = document.getElementById(removeId);
+      elToRemove.remove();
+      this.pushEvent(event, payload);
+    });
+  },
+};
 
-
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks})
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: { _csrf_token: csrfToken },
+  hooks: Hooks,
+});
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
-window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
+window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
-
+window.liveSocket = liveSocket;
